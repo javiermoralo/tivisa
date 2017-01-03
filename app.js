96c8f07d0fdd4952160aec2434f706f56b37df97
J. Javier Moralo <J. Javier Moralo>
2017-01-03 18:40:22 +0100

    Beta 1 - Modificaciones

--- app.js
+++ app.js
@@ -12,14 +12,14 @@ La primera beta se termino el 23 de diciembre de 2016
 var restify = require('restify');
 var builder = require('botbuilder');
 
-var appId = process.env.MICROSOFT_APP_ID;
-var appPassword = process.env.MICROSOFT_APP_PASSWORD;
+//var appId = process.env.MICROSOFT_APP_ID;
+//var appPassword = process.env.MICROSOFT_APP_PASSWORD;
 
 //=========================================================
-// Bot Setup
+// Instalación del Bot
 //=========================================================
 
-// Setup Restify Server
+// Puesta en marcha del Servidor Rest
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function ()
 {
