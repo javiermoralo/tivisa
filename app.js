6ab8a35a9f5a4f24661d341a7fb4e78ac368e074
Javier Moralo <cv@javiermoralo.es>
2017-03-13 17:43:21 +0100

    install botmetrics

--- app.js
+++ app.js
@@ -375,3 +375,31 @@ bot.dialog('/preguntas',
         session.endDialogWithResult();
     }
 ]);
+
+//===================================================================
+//Monitoruzación del Bot
+//===================================================================
+// Remember to run `npm install --save botmetrics` in your app.
+//
+// If you are using an Express-based app, parse the request body
+// and pass along req.body as an argument to Botmetrics
+// for example:
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var Botmetrics = require('botmetrics');
+
+app.use(bodyParser.json()); // for parsing application/json
+
+app.post('/webhooks', function(req, res) {
+  Botmetrics.track(req.body, {
+    apiKey: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo0MDksImV4cCI6MTgwNDk1Mzc5N30.1IknhKrsah8ToDwC_xgY42m2lyb_1DkWE8JIYRgiUaY",
+    botId: "6d878d3cdeb6"
+  });
+  res.status(200).send("");
+});
+
+app.listen(5000, function () {
+  console.log('facebook bot listening on port 5000!');
+});
